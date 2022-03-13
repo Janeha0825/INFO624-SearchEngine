1cb9af287920c0e0a0e0a7447dda0dc88141a1d1
Jane Ha <trangha2508@gmail.com>
2022-03-13 19:47:55 -0400

    added frontend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import response from "./sampleResponse";
+import React, { useState, useEffect } from "react";
+
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   const result =  response.webPages.value;
+  //   setData(result);
+  // })
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("You clicked submit");
+    //set data here
+    const result =  response.webPages.value;
+    setData(result);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header"> Welcome
       </header>
+      <div className="App-body">
+        <form className="form" onSubmit={handleSubmit}>
+          <input type="text" name="query" />
+          <input type="submit" value="Submit" />
+        </form>
+        <div>
+          <table border="2">
+            <tbody>
+              {data != "" ? data.map((item, i) => (
+                <tr key={i}>
+                  <div>{item.name}</div>
+                  <div>{item.snippet}</div>
+                  <div><a href={item.url}>{item.url}</a></div>
+                  <div>---</div>
+                </tr>
+              ) ) : null}
+            </tbody>
+          </table>
+          </div>
+        </div>
     </div>
   );
 }
