c8de56fc64d7a0d8ce62fad23cefe38b653a1750
Tri Le <michaelle7312@gmail.com>
2022-03-15 03:50:49 -0400

    Added auto-suggest

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,70 +7,94 @@ import React, { useState, useEffect } from "react";
 function App() {
 
   const [data, setData] = useState([]);
-
-  // useEffect(() => {
-  //   const result =  response.webPages.value;
-  //   setData(result);
-  // })
-
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log("You clicked submit");
-    var q = document.getElementById("q");
-    console.log(q.value);
-    const result =  response.webPages.value;
-    setData(result);
+  const [query, setQuery] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const hostingPath = "http://localhost:8080";
+  const fetchOptions = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }
   }
 
-  function sendObj() {
-    
-    var q = document.getElementById("q").value;
-    let hostingPath = "http://localhost:8080/"
-    const data = { q: q };
-    console.log(hostingPath + "search?q=" + q)
-    fetch(hostingPath + "search?q=" + q, {
+  function onQueryChange(q) {
+    setQuery(q);
+    fetchSuggestions();
+  }
+
+  function fetchSuggestions() {
+    fetch(`${hostingPath}/suggest?q=${query}`, fetchOptions)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+        setSuggestions(response.suggestionGroups[0].searchSuggestions);
+      });
+  }
+
+  function onSuggestionClick(q) {
+    console.log("q", q);
+    setQuery(q);
+    sendObj(q);
+  }
+
+  function sendObj(q) {
+    setSuggestions([]);
+    fetch(hostingPath + "/search?q=" + q, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Success:', data);
-      setData(data.webPages.value);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+        setData(data.webPages.value);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   }
-  
-  return (
-    <div className="App">
-      <header className="App-header"> Welcome
-      </header>
-      <div className="App-body">
-        {/* <form className="form"> */}
-          <input type="text" name="query" id="q"/>
-          <input type="submit" value="Submit" onClick={sendObj}/>
-        {/* </form> */}
-        <div>
-          <table border="2">
-            <tbody>
-              {data != "" ? data.map((item, i) => (
-                <tr key={i}>
-                  <div>{item.name}</div>
-                  <div>{item.snippet}</div>
-                  <div><a href={item.url}>{item.url}</a></div>
-                  <div>---</div>
-                </tr>
-              ) ) : null}
-            </tbody>
-          </table>
-          </div>
-        </div>
+
+  const suggestionsComponent = () => {
+    return (
+      suggestions && suggestions.map(suggestion => {
+        return (
+          <p key={suggestion.displayText} onClick={() => onSuggestionClick(suggestion.displayText)}>{suggestion.displayText}</p>
+        )
+      })
+    )
+}
+
+return (
+  <div className="App">
+    <header className="App-header"> Welcome
+    </header>
+    <div className="App-body">
+      {/* <form className="form"> */}
+      <input type="text" onChange={e => onQueryChange(e.target.value)} name="query" value={query} />
+
+      {/* Suggestions */}
+      {suggestionsComponent()}
+
+      <input type="submit" value="Submit" onClick={() => sendObj(query)} />
+      {/* </form> */}
+      <div>
+        <table border="2">
+          <tbody>
+            {data != "" ? data.map((item, i) => (
+              <tr key={i}>
+                <div>{item.name}</div>
+                <div>{item.snippet}</div>
+                <div><a href={item.url}>{item.url}</a></div>
+                <div>---</div>
+              </tr>
+            )) : null}
+          </tbody>
+        </table>
+      </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
